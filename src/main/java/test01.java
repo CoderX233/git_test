06c4cdb3feb5d60cb03335186b99c466b09c0e7e
Coder <xyh2298429306@163.com>
2022-05-26 10:32:33 +0800

    first

---
+++ src/main/java/test01.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName test01
+ * @Description TODO
+ * @Author lenovo
+ * @Date 2022-05-26 10:11
+ * @Version 1.0
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println(111111111);
+        System.out.println(222222222);
+    }
+}
